569461214283792708745af3f935aa6a1678f64b
MarceloBRAmaral <marceloamaral.amaral@gmail.com>
2021-02-10 22:03:10 -0500

    Replaced ifs with when.

--- src/Conditionals/Conditionals.kt
+++ src/Conditionals/Conditionals.kt
@@ -1,11 +1,14 @@
 fun main() {
-    val age = 10
-    if (age < 18)
-    {
-        println("You cannot register!")
-    }
-    else
-    {
-        println("You can register!")
+    val age = 21
+    when {
+        age < 18 -> {
+            println("You cannot register!")
+        }
+        age < 21 -> {
+            println("Maybe you can register.")
+        }
+        else -> {
+            println("You can register!")
+        }
     }
 }
